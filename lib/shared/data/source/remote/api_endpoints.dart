395c1f99bc2336c841b91be217f70fa3f416adb4
lemourao <leonardo.lmourao@mercadolivre.com>
2026-02-01 17:52:53 -0300

    deleting unused files

--- lib/shared/data/source/remote/api_endpoints.dart
+++
@@ -1,5 +0,0 @@
-abstract class ApiEndpoints {
-  static final baseUrl = "https://api.artic.edu/api";
-  static final artworks = "$baseUrl/v1/artworks";
-  static String artwork(int id) => "$artworks/$id";
-}
